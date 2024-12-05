7a1acdb18136d2b1325c7e9f456195c2d5812bb2
mdalamin-jbc <jbc.alaminhaque@gmail.com>
2024-12-05 09:43:38 +0600

    nick name reg titile bar fixed

--- src/pages/NickNameReg/NickNameReg.jsx
+++ src/pages/NickNameReg/NickNameReg.jsx
@@ -62,43 +62,46 @@ const NickNameReg = () => {
   };
 
   return (
-    <div className="w-[390px] mx-auto">
+    <div className="">
       <TitleBar title={"スタッフを探す"} />
       <h4 className="mt-4 mb-4 font-semibold font-hiragino text-center">
         ニックネーム（表示名）をご登録ください
       </h4>
+      <div >
+        <div className="flex flex-col justify-center">
+          <form
+            className="flex flex-col w-[342px] mx-auto"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <div className="form-control">
+              <input
+                {...register("name", { required: "Name is required" })}
+                name="name"
+                type="text"
+                placeholder="名前"
+                className="input rounded-[5px] py-4 mt-1 mb-[9px] w-full pl-4 font-Noto text-[#44495B80] text-sm border-2 border-[#D9D9D9] focus:border-[#707070] focus:outline-none"
+                defaultValue={user?.name}
+              />
+              {errors.name && (
+                <span className="text-[#F43C3C]  text-sm mt-2">
+                  {errors.name.message}
+                </span>
+              )}
+              {error?.name && (
+                <span className="text-[#F43C3C] text-sm mt-2">
+                  {error.name}
+                </span>
+              )}
+            </div>
 
-      <div className="flex flex-col justify-center">
-        <form
-          className="flex flex-col w-[342px] mx-auto"
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <div className="form-control">
-            <input
-              {...register("name", { required: "Name is required" })}
-              name="name"
-              type="text"
-              placeholder="名前"
-              className="input rounded-[5px] py-4 mt-1 mb-[9px] w-full pl-4 font-Noto text-[#44495B80] text-sm border-2 border-[#D9D9D9] focus:border-[#707070] focus:outline-none"
-              defaultValue={user?.name}
-            />
-            {errors.name && (
-              <span className="text-[#F43C3C]  text-sm mt-2">
-                {errors.name.message}
-              </span>
-            )}
-            {error?.name && (
-              <span className="text-[#F43C3C] text-sm mt-2">{error.name}</span>
-            )}
-          </div>
-
-          <button className="fixed bottom-[130px]">
-            <ButtonPrimary
-              btnText="新規登録"
-              style="bg-gradient-to-r from-[#65D0F2] to-[#2399F4] w-[342px] rounded-full font-hiragino text-center py-[12px] font-bold text-white"
-            />
-          </button>
-        </form>
+            <button className="fixed bottom-[130px]">
+              <ButtonPrimary
+                btnText="新規登録"
+                style="bg-gradient-to-r from-[#65D0F2] to-[#2399F4] w-[342px] rounded-full font-hiragino text-center py-[12px] font-bold text-white"
+              />
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
