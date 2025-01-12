56397353b1e82e483b2cc586c0c43f68d7e54f90
mdalamin-jbc <jbc.alaminhaque@gmail.com>
2025-01-12 13:56:26 +0600

    days

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    include: ['dayjs']
-  },
-  build: {
-    rollupOptions: {
-      external: ['dayjs']
-    }
-  }
+  plugins: [react()]
 });
