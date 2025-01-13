fd1864b0d1004c69764d9764688360103e1f8664
mdalamin-jbc <jbc.alaminhaque@gmail.com>
2025-01-13 12:13:51 +0600

    new

--- src/pages/BillingScreen/BillingScreen.jsx
+++ src/pages/BillingScreen/BillingScreen.jsx
@@ -144,8 +144,8 @@ const BillingScreen = () => {
       // amount: 1000,
       currency: "JPY",
       payment_method: "paypal",
-      return_url: `https://throwin.eswaap.com/staff/${staff?.username}/chargeCompleted`,
-      cancel_url: "https://throwin.eswaap.com/payment-cancle",
+      return_url: `https://main.d1qin9n9dqg0zp.amplifyapp.com/staff/${staff?.username}/chargeCompleted`,
+      cancel_url: "https://main.d1qin9n9dqg0zp.amplifyapp.com/payment-cancle",
     });
   }, [
     persAmount,
