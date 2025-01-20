5d933e264aff5b549e771edcdb05d5b315812f75
mdalamin-jbc <jbc.alaminhaque@gmail.com>
2025-01-20 23:44:50 +0600

    sataff page ui fixed

--- src/components/StaffProfileCard/StaffProfileCard.jsx
+++ src/components/StaffProfileCard/StaffProfileCard.jsx
@@ -11,13 +11,13 @@ const StaffProfileCard = ({
 }) => {
   return (
     <>
-      <div className="relative max-w-[430px]">
+      <div className="relative w-full max-w-[430px]">
         <img
           src="https://i.postimg.cc/HLdQr5yp/5e3ca18b58c181ccc105ca95163e891c.jpg"
           alt={`${staff?.name} image`}
-          className="object-cover  w-full h-[277px]"
+          className="object-cover w-full h-[277px]"
         />
-
+  
         <div className="absolute bottom-0 left-0 w-full px-6 mb-[22px] p-2 text-white rounded-b-lg">
           <div className="flex justify-between items-center">
             <div className="bg-white text-[#F06464] flex items-center gap-1 px-2 py-1 rounded-full shadow-md">
@@ -36,13 +36,14 @@ const StaffProfileCard = ({
           </div>
         </div>
       </div>
-
-      <div className="bg-[#80D0E91A] pt-5 pb-[17px] px-[26px] max-w-[430px]">
+  
+      <div className="bg-[#80D0E91A] pt-5 pb-[17px] px-[26px] w-full max-w-[430px]">
         <h2 className="font-semibold text-lg mb-2">自己紹介</h2>
         <p className="font-light text-sm">{staff?.introduction}</p>
       </div>
     </>
   );
+  
 };
 
 StaffProfileCard.propTypes = {
