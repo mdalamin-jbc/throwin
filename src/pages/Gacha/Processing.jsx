e0b20f72321136cafc6940713c3fd328b734d26c
mdalamin-jbc <jbc.alaminhaque@gmail.com>
2025-02-04 00:00:38 +0600

    gacha api update

--- src/pages/Gacha/Processing.jsx
+++ src/pages/Gacha/Processing.jsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import TitleBar from "../../components/TitleBar";
 import { RiArrowLeftSLine } from "react-icons/ri";
 import logo from "../../assets/images/home/logo.png";
@@ -29,15 +29,14 @@ const Processing = () => {
         icon={<img className="w-[110px] items-center" src={logo} alt="logo" />}
       />
       <div className="max-w-[430px] mx-auto">
-        <div className="flex justify-center">
-          <video
-            ref={videoRef}
-            src={video}
-            className=""
-            autoPlay
-            onEnded={handleVideoEnd}
-          />
-        </div>
+        <video
+          ref={videoRef}
+          src={video}
+          className="overflow-auto"
+          autoPlay
+          onEnded={handleVideoEnd}
+        />
+        <div className=""></div>
       </div>
     </div>
   );
--- src/pages/Gacha/UseTicket.jsx
+++ src/pages/Gacha/UseTicket.jsx
@@ -60,13 +60,18 @@ const UseTicket = () => {
         <div className="flex justify-center">
           <img src={img} alt="Gacha Machine" />
         </div>
-        <div className="flex justify-between items-center font-bold text-base border border-[#49BBDF] rounded-lg shadow-md px-5 py-4 bg-[#EAF8FD]">
-          <h3 className="text-[#585858]">{ticket.store_name}</h3>
-          <h4 className="flex gap-1 items-center border-l-2 border-[#49BBDF] border-dashed pl-5 text-[#585858]">
-            x<span className="text-2xl font-bold">{ticket.available_spin}</span>
-          </h4>
+        <div className="mx-8">
+          <div className="flex justify-between items-center font-bold text-base border border-[#49BBDF] rounded-lg shadow-md px-5 py-4 bg-[#EAF8FD]">
+            <h3 className="text-[#585858]">{ticket.store_name}</h3>
+            <h4 className="flex gap-1 items-center border-l-2 border-[#49BBDF] border-dashed pl-5 text-[#585858]">
+              x
+              <span className="text-2xl font-bold">
+                {ticket.available_spin}
+              </span>
+            </h4>
+          </div>
         </div>
-        <div className="mt-[43px] relative">
+        <div className="mt-[43px] relative ">
           <div className="absolute left-1/2 transform -translate-x-1/2 -mt-5 w-[240px] rounded text-center">
             <h4 className="w-full text-[#36ABE0] relative rounded-full bg-white p-[2px]">
               <span className="absolute inset-0 bg-gradient-to-r from-[#3BAFE0] to-[#209AE1] rounded-full"></span>
